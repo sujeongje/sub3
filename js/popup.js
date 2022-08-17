9d3de22b904394183bc38774504607459a66ff10
sujeongje <111342345+sujeongje@users.noreply.github.com>
2022-08-17 15:56:18 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,51 @@
+$(function(){
+    $('.pop1 a').click(function(){
+        $('.popup1').fadeIn();
+    });
+
+    $('.popup1 a').click(function(){
+        $('.popup1').fadeOut();
+    });
+});
+
+$(function(){
+    $('.pop2 a').click(function(){
+        $('.popup2').fadeIn();
+    });
+
+    $('.popup2 a').click(function(){
+        $('.popup2').fadeOut();
+    });
+});
+
+$(function(){
+    $('.pop3 a').click(function(){
+        $('.popup3').fadeIn();
+    });
+
+    $('.popup3 a').click(function(){
+        $('.popup3').fadeOut();
+    });
+});
+
+
+$(function(){
+    $('.pop4 a').click(function(){
+        $('.popup4').fadeIn();
+    });
+
+    $('.popup4 a').click(function(){
+        $('.popup4').fadeOut();
+    });
+});
+
+
+$(function(){
+    $('.pop5 a').click(function(){
+        $('.popup5').fadeIn();
+    });
+
+    $('.popup5 a').click(function(){
+        $('.popup5').fadeOut();
+    });
+});
